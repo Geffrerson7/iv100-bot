4e8ebab3e9ec56c9ddb3711b9105b1f17c99df91
Gefferson Casasola <gcasasolah@uni.pe>
2024-03-15 15:23:56 -0500

    Update handlers.py

--- bot/handlers.py
+++ bot/handlers.py
@@ -24,23 +24,26 @@ logger = logging.getLogger(__name__)
 async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global is_start_active
 
-    if not is_start_active:
-        user_name = update.effective_user.first_name
-        message_text = f"¡Hola {user_name}, bienvenido a Adventure Elements!\n"
-        message_text += "Este es un menú explicativo:\n\n"
-        message_text += "/iv100 - Inicia el bot.\n"
-        message_text += "/stop - Detiene el bot.\n"
-
-        keyboard = [["/iv100", "/stop"]]
-        reply_markup = ReplyKeyboardMarkup(
-            keyboard, one_time_keyboard=True, resize_keyboard=True
-        )
-
-        await update.message.reply_text(message_text, reply_markup=reply_markup)
-    else:
+    # Verificar si ya hay una instancia activa del bot
+    if is_start_active:
         await update.message.reply_text(
-            "Las coordenadas ya se están enviando. Si deseas detener el envío de coordenadas, usa /stop"
+            "Lo siento, ya hay una instancia activa del bot. Por favor, espera a que se detenga antes de iniciar otra."
         )
+        return
+
+    # Si no hay instancia activa, proceder con el manejo del mensaje
+    user_name = update.effective_user.first_name
+    message_text = f"¡Hola {user_name}, bienvenido a Adventure Elements!\n"
+    message_text += "Este es un menú explicativo:\n\n"
+    message_text += "/iv100 - Inicia el bot.\n"
+    message_text += "/stop - Detiene el bot.\n"
+
+    keyboard = [["/iv100", "/stop"]]
+    reply_markup = ReplyKeyboardMarkup(
+        keyboard, one_time_keyboard=True, resize_keyboard=True
+    )
+
+    await update.message.reply_text(message_text, reply_markup=reply_markup)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -48,6 +51,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     if not is_start_active:
         try:
+            is_start_active = True  # Marcamos que la tarea está activa
             total_text = send_pokemon_data()
 
             if total_text:
@@ -56,16 +60,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                         chat_id=update.effective_chat.id, text=text
                     )
                     await asyncio.sleep(2)
-                is_start_active = True
-            else:
-                await update.message.reply_text(
-                    f"{update.effective_user.first_name} espera unos segundos mientras encontramos pokemons"
-                )
         except Exception as e:
             print(f"Error en send_pokemon_data(): {e}")  # Imprime el error
             await update.message.reply_text(
                 "Ocurrió un error al obtener los datos de los Pokémon. Por favor, inténtalo de nuevo más tarde."
             )
+        finally:
+            is_start_active = False  # Marcamos que la tarea ha terminado
     else:
         await update.message.reply_text(
             "Las coordenadas ya se están enviando. Si deseas detener el envío de coordenadas, usa /stop"
@@ -86,7 +87,9 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
 
     logger.error("Exception while handling an update:", exc_info=context.error)
 
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
     tb_string = "".join(tb_list)
 
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
@@ -103,33 +106,25 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
     )
 
 
-# def run_bot():
-#     logging.basicConfig(
-#         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-#         level=logging.ERROR,
-#     )
+def run_bot():
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.ERROR,
+    )
 
-#     application = Application.builder().token(token).build()
+    application = Application.builder().token(token).build()
 
-#     application.add_error_handler(error_handler)
-
-#     application.add_handler(CommandHandler("iv100", start))
-#     application.add_handler(CommandHandler("stop", stop))
-#     application.add_handler(
-#         MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler)
-#     )
-
-#     try:
-#         print("El Bot de Telegram ahora se ejecutará en modo de run_polling.")
-#         application.run_polling(allowed_updates=Update.ALL_TYPES)
-#     except Exception as e:
-#         logging.error("An error occurred during polling: {e}", exc_info=True)
-#         traceback.print_exc()
-
-def setup_handlers(application):
     application.add_error_handler(error_handler)
+
     application.add_handler(CommandHandler("iv100", start))
     application.add_handler(CommandHandler("stop", stop))
     application.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler)
     )
+
+    try:
+        print("El Bot de Telegram ahora se ejecutará en modo de run_polling.")
+        application.run_polling(allowed_updates=Update.ALL_TYPES)
+    except Exception as e:
+        logging.error("An error occurred during polling: {e}", exc_info=True)
+        traceback.print_exc()
--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-# from bot.handlers import run_bot
+from bot.handlers import run_bot
 
-
-# def main():
-#     run_bot()
-
-
-# if __name__ == "__main__":
-#     main()
-from bot.handlers import setup_handlers
-from telegram.ext import Application  # Suponiendo que tengas un módulo application.py donde defines la configuración de tu aplicación
-from telegram import Update
-import traceback
-import logging
-from data import config
-
-token = config.token
 
 def main():
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.ERROR,
-    )
+    run_bot()
 
-    application = Application.builder().token(token).build()
-    setup_handlers(application)  # Configura los manejadores de eventos en la aplicación
-    try:
-        print("El Bot de Telegram ahora se ejecutará en modo de run_polling.")
-        application.run_polling(allowed_updates=Update.ALL_TYPES)
-    except Exception as e:
-        logging.error("An error occurred during polling: {e}", exc_info=True)
-        traceback.print_exc()
 
 if __name__ == "__main__":
     main()
