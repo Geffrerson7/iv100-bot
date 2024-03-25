fc95c835d6b7a40f95d98c62bf7450ae74f0f203
Gefferson Casasola <gcasasolah@uni.pe>
2024-03-25 13:54:24 -0500

    Add error handler

--- bot/service.py
+++ bot/service.py
@@ -186,7 +186,7 @@ def generate_pokemon_messages():
                         message = (
                             f"*🄰* `{name}` {shiny_icon}{gender_icon}\n"
                             f"*🄴* IV:💯 ᴄᴘ:{cp} LV:{level}\n"
-                            f"{move1_icon}{move1} / {move2_icon}{move2}\n"
+                            f"{move1_icon}{move1} \| {move2_icon}{move2}\n"
                             f"*🌀☄️Tᴏᴘ💯Gᴀʟᴀxʏ☄️🌀*\n"
                             f"⌚ᴅsᴘ {dsp}\n"
                             f"`{latitude},{longitude}`"
